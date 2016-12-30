11928273547751d2ef3e31fe625578995c6bfc0d
Achim D. Brucker <adbrucker@0x5f.org>
2016-12-30 15:14:47 +0000

    Fixed section structure.

--- FWNormalisation/FWNormalisation.thy
+++ FWNormalisation/FWNormalisation.thy
@@ -34,7 +34,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************)
 
-chapter {* Firewall Policy Normalisation *}
+section {* Firewall Policy Normalisation *}
 theory  
   FWNormalisation
   imports 
--- PacketFilter/PacketFilter.thy
+++ PacketFilter/PacketFilter.thy
@@ -34,7 +34,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************)
-subsection {* Network Policies: Packet Filter *}
+section {* Network Policies: Packet Filter *}
 theory 
   PacketFilter
   imports
